382d171d5939adbf155c29ba23fe023939b6d803
Zhao Cheng <zcbenz@gmail.com>
2012-10-20 13:42:08 +0800

    Add menu demo.

---
+++ menus/README.md
@@ -0,0 +1,11 @@
+# Menus
+
+This is a basic demo that demonstrates the flexibility of node-webkit's Menus
+API, there aree three menus bound to three areas in the page.
+
+**This demo requires node-webkit >= v0.3.0.**
+
+## APIs
+
+* [Menu](https://github.com/rogerwang/node-webkit/wiki/Menu)
+* [MenuItem](https://github.com/rogerwang/node-webkit/wiki/MenuItem)
---
+++ menus/script.js
@@ -0,0 +1,66 @@
+var gui = require('nw.gui');
+gui.Window.get().show();
+
+var menu1 = new gui.Menu();
+var submenu = new gui.Menu();
+submenu.append(new gui.MenuItem({ type: 'checkbox', label: 'box1' }));
+submenu.append(new gui.MenuItem({ type: 'checkbox', label: 'box2' }));
+submenu.append(new gui.MenuItem({ type: 'checkbox', label: 'box3' }));
+submenu.append(new gui.MenuItem({ type: 'checkbox', label: 'box4' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/cut.png', label: 'Cut' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/edit.png', label: 'Edit' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/email.png', label: 'Email' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/play.png', label: 'Play' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/tick.png', label: 'Tick' }));
+menu1.append(new gui.MenuItem({ type: 'separator' }));
+menu1.append(new gui.MenuItem({ icon: 'imgs/disk.png', label: 'Disk', submenu: submenu }));
+
+var menu2 = new gui.Menu();
+menu2.append(new gui.MenuItem({ type: 'checkbox', icon: 'imgs/apple.png', label: 'Apple' }));
+menu2.append(new gui.MenuItem({ type: 'checkbox', icon: 'imgs/banaba.png', label: 'Banana' }));
+menu2.append(new gui.MenuItem({ type: 'checkbox', icon: 'imgs/strawberry.png', label: 'Strawberry' }));
+menu2.append(new gui.MenuItem({ type: 'checkbox', icon: 'imgs/pear.png', label: 'Pear' }));
+menu2.append(new gui.MenuItem({ type: 'separator' }));
+var info_item = new gui.MenuItem({ label: 'Which Fruit Do I Love?' });
+menu2.append(info_item);
+var lastone = null;
+function flip() {
+  if (lastone) {
+    lastone.checked = false;
+    lastone.enabled = true;
+  }
+
+  lastone = this;
+  this.enabled = false;
+  info_item.label = 'I Love ' + this.label;
+  info_item.icon = this.icon;
+}
+for (var i = 0; i < 4; ++i)
+  menu2.items[i].click = flip;
+
+var menu3 = new gui.Menu();
+var colors = [ '#000000', '#FF0000', '#00FF00' , '#0000FF', '#FFFF00', '#00FFFF', '#FF00FF', '#C0C0C0', '#FFFFFF' ];
+function changecolor() {
+  document.getElementById('area-3').style.backgroundColor = this.label;
+}
+for (var i = 0; i < colors.length; i++) {
+  menu3.append(new gui.MenuItem({ label: colors[i], click: changecolor }));
+};
+
+document.getElementById('area-1').addEventListener('contextmenu', function(ev) { 
+  ev.preventDefault();
+  menu1.popup(ev.x, ev.y);
+  return false;
+});
+
+document.getElementById('area-2').addEventListener('contextmenu', function(ev) { 
+  ev.preventDefault();
+  menu2.popup(ev.x, ev.y);
+  return false;
+});
+
+document.getElementById('area-3').addEventListener('contextmenu', function(ev) { 
+  ev.preventDefault();
+  menu3.popup(ev.x, ev.y);
+  return false;
+});
