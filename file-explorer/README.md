8479f4fda85e9d4fc0fb216c4dc96ca82f1fa379
Zhao Cheng <zcbenz@gmail.com>
2012-11-04 15:19:24 +0800

    Add file-explorer back (with jade).

---
+++ file-explorer/README.md
@@ -0,0 +1,16 @@
+# File Explorer
+
+A simple file explorer implemented with node.js modules, you can navigate through
+folders and open files.
+
+## Screen shot
+
+![screenshot](http://ww4.sinaimg.cn/large/6556d357tw1dxui6m0i9kj.jpg)
+
+## APIs
+
+* [fs module](http://nodejs.org/api/fs.html)
+* [events module](http://nodejs.org/api/events.html)
+* [path module](http://nodejs.org/api/path.html)
+* [util module](http://nodejs.org/api/util.html)
+* [child_process module](http://nodejs.org/api/child_process.html)
---
+++ file-explorer/main.js
@@ -0,0 +1,26 @@
+global.$ = $;
+
+var abar = require('address_bar');
+var folder_view = require('folder_view');
+var path = require('path');
+var shell = require('nw.gui').Shell;
+
+$(document).ready(function() {
+  var folder = new folder_view.Folder($('#files'));
+  var addressbar = new abar.AddressBar($('#addressbar'));
+
+  folder.open(process.cwd());
+  addressbar.set(process.cwd());
+
+  folder.on('navigate', function(dir, mime) {
+    if (mime.type == 'folder') {
+      addressbar.enter(mime);
+    } else {
+      shell.openItem(mime.path);
+    }
+  });
+
+  addressbar.on('navigate', function(dir) {
+    folder.open(dir);
+  });
+});
